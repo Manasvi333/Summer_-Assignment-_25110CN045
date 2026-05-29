6276f7315921d908f49eadca6ac0bee66bfd81b9
Manasvi333 <manasvi032006@gmail.com>
2026-05-29 12:54:24 +0530

    reverse of a number

---
+++ Day2/Q6/reverse.java
@@ -0,0 +1,16 @@
+package Day2.Q6;
+import java.util.Scanner;
+public class reverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        int rev = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            rev = rev * 10 + rem;
+            n = n / 10;
+        }
+        System.out.println("Reversed number: " + rev);
+    }
+}

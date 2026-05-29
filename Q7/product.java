b19948f5f5136500db6e354a9152cf15f9e27609
Manasvi333 <manasvi032006@gmail.com>
2026-05-29 13:04:13 +0530

    product of the given number

---
+++ Q7/product.java
@@ -0,0 +1,17 @@
+package Q7;
+import java.util.Scanner;
+public class product { 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int product = 1;
+        while (n > 0) {
+            int rem = n % 10;
+            product = product * rem;
+            n = n / 10;
+        }
+        System.out.println("Product of the digits of the given number = " + product);
+    }
+    
+}

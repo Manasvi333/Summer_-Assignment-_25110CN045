8e8b4ae2e4e2c9d9dd8a39c4c3f5e51a4dcd4ef1
Manasvi333 <manasvi032006@gmail.com>
2026-05-30 12:22:31 +0530

    LCM of two numbers

---
+++ Day3/prime_number/leastcommonmultiple/LCM.java
@@ -0,0 +1,25 @@
+package Day3.prime_number.leastcommonmultiple;
+import java.util.Scanner;
+public class LCM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+        int lcm = findLCM(num1, num2);
+        System.out.println("The least common multiple of " + num1 + " and " + num2 + " is: " + lcm);
+        sc.close();
+    }
+
+    public static int findLCM(int a, int b) {
+        return (a * b) / findGCD(a, b);
+    }
+
+    public static int findGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return findGCD(b, a % b);
+    }
+}

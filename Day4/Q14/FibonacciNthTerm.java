deb2e4e53758f8629a43b8f22ff500a46bf3b07a
Manasvi333 <manasvi032006@gmail.com>
2026-05-31 14:05:04 +0530

    Nth term fibonacci series

---
+++ Day4/Q14/FibonacciNthTerm.java
@@ -0,0 +1,27 @@
+package Day4.Q14;
+import java.util.Scanner;
+public class FibonacciNthTerm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1, c = 0;
+
+        if (n == 1) {
+            System.out.println("Nth term = " + a);
+        } else if (n == 2) {
+            System.out.println("Nth term = " + b);
+        } else {
+            for (int i = 3; i <= n; i++) {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            System.out.println("Nth term = " + c);
+        }
+
+        sc.close();
+    }
+}

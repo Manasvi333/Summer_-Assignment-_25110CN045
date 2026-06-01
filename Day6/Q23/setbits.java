cf5dc995944ef0323bc83e1ae4b59e85b3bdd34a
Manasvi333 <manasvi032006@gmail.com>
2026-06-01 22:08:07 +0530

    Set of Bits

---
+++ Day6/Q23/setbits.java
@@ -0,0 +1,16 @@
+package Day6.Q23;
+import java.util.Scanner;
+public class setbits {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n =sc.nextInt();
+        int count = 0;
+        while (n > 0) {
+            count += n & 1;
+            n >>= 1;
+        }
+        System.out.println("Number of set bits: " + count);
+        sc.close();
+    }
+}

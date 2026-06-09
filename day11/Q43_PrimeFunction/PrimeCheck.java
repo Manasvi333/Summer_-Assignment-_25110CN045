e25c79589c1203b3240ba4fff08ffb74ffc8b003
Manasvi333 <manasvi032006@gmail.com>
2026-06-09 12:15:37 +0530

    Add function for checking prime number

--- day11/Q43_PrimeFunction/PrimeCheck.java
+++ day11/Q43_PrimeFunction/PrimeCheck.java
@@ -12,7 +12,7 @@ public class PrimeCheck {
             return false;
         }
         for (int i = 5; i * i <= n; i += 6) {
-            if (n % i == 0 || n % (i + 2) == 0) {
+            if (n % i == 0 || n % (i + 2) == 0) { 
                 return false;
             }
         }

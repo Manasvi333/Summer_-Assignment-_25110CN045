dce7c746ad0b051edf944868a8c015b9736a1639
Manasvi333 <manasvi032006@gmail.com>
2026-06-10 16:04:45 +0530

    Palindrome check function

---
+++ Day12/Q45/PalindromeCheck.java
@@ -0,0 +1,28 @@
+package Day12.Q45;
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static boolean isPalindrome(int number) {
+        int original = number;
+        int reversed = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+        return original == reversed;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        if (isPalindrome(num)) {
+            System.out.println(num + " is a palindrome");
+        } else {
+            System.out.println(num + " is not a palindrome");
+        }
+    }
+}

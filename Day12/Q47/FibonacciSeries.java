878475bcada5782f949f5d211ecc134bdd16bcf0
Manasvi333 <manasvi032006@gmail.com>
2026-06-11 11:46:56 +0530

    Fibonacci function

---
+++ Day12/Q47/FibonacciSeries.java
@@ -0,0 +1,31 @@
+package day12.Q47;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+    public static void printFibonacci(int n) {
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci Series: ");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+
+        printFibonacci(n);
+
+        scanner.close();
+    }
+}

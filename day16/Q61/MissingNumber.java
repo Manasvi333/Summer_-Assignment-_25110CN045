ee5dbd9523fef37384bf7959d52685fd01302ae5
Manasvi333 <manasvi032006@gmail.com>
2026-06-15 11:00:44 +0530

    Missing number in an array

---
+++ day16/Q61/MissingNumber.java
@@ -0,0 +1,32 @@
+package day16.Q61; 
+import java.util.Scanner;
+
+public class MissingNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n (maximum number): ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n - 1];
+
+        System.out.println("Enter " + (n - 1) + " numbers:");
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int num : arr) {
+            actualSum += num;
+        }
+
+        int missing = expectedSum - actualSum;
+
+        System.out.println("Missing number is: " + missing);
+
+        sc.close();
+    }
+}

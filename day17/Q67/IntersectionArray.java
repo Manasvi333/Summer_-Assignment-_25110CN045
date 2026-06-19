d362a452be89dad42defb06428d8d16fde30e7b2
Manasvi333 <manasvi032006@gmail.com>
2026-06-19 21:46:05 +0530

    Intersection of two arrays

---
+++ day17/Q67/IntersectionArray.java
@@ -0,0 +1,52 @@
+package day17.Q67;
+import java.util.Scanner;
+
+public class IntersectionArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of first array: ");
+        int n1 = sc.nextInt();
+        int[] arr1 = new int[n1];
+
+        System.out.println("Enter elements of first array:");
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter size of second array: ");
+        int n2 = sc.nextInt();
+        int[] arr2 = new int[n2];
+
+        System.out.println("Enter elements of second array:");
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        System.out.println("Intersection of Arrays:");
+
+        for (int i = 0; i < n1; i++) {
+            boolean alreadyPrinted = false;
+
+            for (int k = 0; k < i; k++) {
+                if (arr1[i] == arr1[k]) {
+                    alreadyPrinted = true;
+                    break;
+                }
+            }
+
+            if (alreadyPrinted)
+                continue;
+
+            for (int j = 0; j < n2; j++) {
+                if (arr1[i] == arr2[j]) {
+                    System.out.print(arr1[i] + " ");
+                    break;
+                }
+            }
+        }
+
+        sc.close();
+    }
+}

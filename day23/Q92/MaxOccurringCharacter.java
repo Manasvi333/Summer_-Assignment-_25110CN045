1ef3f2482ca3065c1fb5953a5e1acf8299df12e5
Manasvi333 <manasvi032006@gmail.com>
2026-06-23 11:30:11 +0530

    Find maximum occurring character

---
+++ day23/Q92/MaxOccurringCharacter.java
@@ -0,0 +1,41 @@
+package day23.Q92;
+import java.util.Scanner;
+
+public class MaxOccurringCharacter {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        char maxChar = ' ';
+        int maxCount = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (ch == ' ')
+                continue;
+
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (ch == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count > maxCount) {
+                maxCount = count;
+                maxChar = ch;
+            }
+        }
+
+        System.out.println("Maximum Occurring Character = " + maxChar);
+        System.out.println("Frequency = " + maxCount);
+
+        sc.close();
+    }
+}

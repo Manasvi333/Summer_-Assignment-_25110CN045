b771551a734479b857ce5deb6310f228b0f0f0b4
Manasvi333 <manasvi032006@gmail.com>
2026-06-23 11:27:49 +0530

    Check anagram strings

---
+++ day23/Q91/AnagramCheck.java
@@ -0,0 +1,33 @@
+package day23.Q91;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramCheck {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine().toLowerCase();
+
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine().toLowerCase();
+
+        str1 = str1.replace(" ", "");
+        str2 = str2.replace(" ", "");
+
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        if (Arrays.equals(arr1, arr2)) {
+            System.out.println("Strings are Anagrams");
+        } else {
+            System.out.println("Strings are Not Anagrams");
+        }
+
+        sc.close();
+    }
+}

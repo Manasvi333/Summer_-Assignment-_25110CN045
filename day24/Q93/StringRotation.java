17ebd76882997b2bf73943aa099a3527e7834a2c
Manasvi333 <manasvi032006@gmail.com>
2026-06-24 15:40:34 +0530

    Check string rotation

---
+++ day24/Q93/StringRotation.java
@@ -0,0 +1,25 @@
+package day24.Q93;
+import java.util.Scanner;
+
+public class StringRotation {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine();
+
+        if (str1.length() == str2.length() &&
+            (str1 + str1).contains(str2)) {
+
+            System.out.println("String is a Rotation");
+        } else {
+            System.out.println("String is Not a Rotation");
+        }
+
+        sc.close();
+    }
+}
